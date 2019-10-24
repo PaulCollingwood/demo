2022d056aa6483c77e8d56984d2a31cf8e524cbc
Paul <paul@collingwood.io>
2019-10-24 12:12:53 +0100

    Set up Chromedriver

---
+++ features/environment.py
@@ -0,0 +1,24 @@
+""" Configure the Behave Instance on starup """
+
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+
+HOST = 'http://automationpractice.com'
+
+
+def find_by_xpath(self, xpath):
+    return self._web_driver_wait.until(ec.visibility_of_element_located((By.XPATH, xpath)))
+
+
+def before_all(context):
+    context.host = HOST
+    context.webdriver = webdriver.Chrome(executable_path='./chromedriver')
+
+
+def after_all(context):
+    context.webdriver.quit()
+
+
+
